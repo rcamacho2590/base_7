60677928ee56722ff7cfcbb21f5a45aaa0890f7a
Rafael Camacho <rafael@softwarecriollo.com>
2016-06-07 12:35:16 -0400

    controller end point added, route added, number model fix

---
+++ app/controllers/numbers_controller.rb
@@ -0,0 +1,12 @@
+class NumbersController < ApplicationController
+  respond_to :json
+
+  def base_seven
+    @number = Number.number_base7(params[:id])
+    render :status => 200,
+     :json => { :success => true,
+          :number => @number
+        }
+  end
+
+end
