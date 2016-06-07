85aff00cfd7a9868e3156494025be5f42cc81ea9
Rafael Camacho <rafael@softwarecriollo.com>
2016-06-07 11:34:45 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'rake', "10.5.0"
+gem 'cucumber'
---
+++ app/models/number.rb
@@ -0,0 +1,12 @@
+puts 'loaded!!!!!!!!'
+
+class Number
+  TABLE = {6=>"B", 5=>"L", 4=>"A", 3=>"N", 2=>"C", 1=>"I", 0=>"K"}
+
+  def number_base7(num)
+    letters = ""
+    numb7 = num.to_s(7)
+    numb7.chars.map { |n| letters.concat TABLE[n.to_i] }
+    letters
+  end
+end
